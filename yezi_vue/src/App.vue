102d3e80aba981afd764271938fcc5fc0f00c60a
yezisuper <1016232959@qq.com>
2017-08-15 17:26:11 +0800

    vue

---
+++ yezi_vue/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <!-- <img src="./assets/logo.png"> -->
+    <hello></hello>
+  </div>
+</template>
+
+<script>
+import hello from './components/hello'
+
+export default {
+  name: 'app',
+  components: {
+    hello
+  }
+}
+</script>
+
+<style>
+*{
+  margin: 0;padding: 0px;
+}
+ul,li{
+  list-style: none;
+}
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+
+</style>
---
+++ yezi_vue/src/components/nav.vue
@@ -0,0 +1,96 @@
+<template>
+<div class="nav">
+	<div class="nav-head">
+			<div class="nav-head-con">
+				<div class="nav-head-item">
+					<a href="" class="logo fl"></a>
+					<a href="/index.php/customer/index.html" class="quit">{{ msg }}</a>
+					<ul class="nav-list" id="tabs_nav">
+						<li><a href="">健康管理</a></li>
+						<li><a href="">我的教练</a></li>
+						<li><a href="">个人中心</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+</div>
+</template>
+<script>
+export default {
+  name: 'nav',
+  data () {
+    return {
+      msg: '退出'
+    }
+  }
+}
+</script>
+<style scoped>
+.nav-head{
+	width: 100%;
+	min-width:1200px;
+	height: 50px;
+	background: #FFFFFF;
+	position: relative;
+}
+.nav-head-con {
+	width: 100%;
+	height: 100%;
+	position: absolute;
+	box-shadow: 0 0 10px rgba(0,0,0,.3);
+	z-index: 10;
+	background-color: #fff;
+}
+.nav-head-item .logo{
+	margin-top:8px;
+	/* background: url(./assets/logo.png) no-repeat; */
+	display: block;
+	width: 221px;
+	height: 40px;
+
+}
+.nav-head-item{
+	width: 1200px;
+	margin: 0 auto;
+	clear: both;
+	overflow: hidden;
+	zoom: 1;
+	height:50px;
+	line-height: 50px;
+}
+.nav-list{
+	float: right;
+}
+.nav-list li{
+	float: left;
+	padding: 0 32px;
+}
+.nav-list li a{
+	float: left;
+	color: #546476;
+	text-decoration: none;
+	font-size: 18px;
+}
+.nav-list li a.on{
+	color: #47cce2;
+}
+.nav-head-item>p {
+	float: right;
+	width: 55px;
+	height: 68px;
+	line-height: 70px;
+	border-bottom: 2px solid #36b4f9;
+	text-align: center;
+	margin-right: 75px;
+	box-sizing: content-box;
+}
+.quit{
+	float: right;
+	color: #546476;
+    text-decoration: none;
+    font-size: 18px;
+    font-family: 'Microsoft Yahei' ;
+    margin-right: 35px;
+    margin-left:100px;
+}
+</style>
